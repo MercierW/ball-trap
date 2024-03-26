2d0400849e052b60b4c0232290c5767b79cf9f14
William Mercier <59589490+MercierW@users.noreply.github.com>
2024-03-26 17:15:49 +0100

    Delete Jeu-Ifocop-2024/back-end directory

--- Jeu-Ifocop-2024/back-end/src/db/client.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-import { MongoClient } from 'mongodb';
-
-// Connection URL
-const url = String(process.env.MONGOURL);
-const client = new MongoClient(url);
-
-export default client;
